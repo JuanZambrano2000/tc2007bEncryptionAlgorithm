b0a50d53c5eff4c11ee943c7c486ffc3928851f5
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-21 16:14:00 -0600

    Update page.tsx

--- tc2007b_encryption_algorithm/src/app/page.tsx
+++ tc2007b_encryption_algorithm/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import Navbar from './components/navbar';
 
@@ -9,7 +10,15 @@ export default function Home() {
         <header className="text-4xl mb-8 px-12 py-8">Activity 2 - Design of a Symmetric Encryption Algorithm</header>
         <div className="flex flex-col min-h-screen px-12">
           <p className="text-lg mb-4">
-          Our team is tasked with discussing the fundamental characteristics that a robust symmetric encryption algorithm must possess. These characteristics include diffusion and confusion, and you need to design a didactic symmetric encryption algorithm that incorporates basic operations like bit shifting, transposition, substitution, XOR, and multiple rounds to enhance security. The algorithm should also consider reversible operations and involve complex operations like multiplication and key rotations if possible. After designing the algorithm, you'll need to implement it in a language of your choice, preferably one that supports bit-level operations. The program should encrypt alphanumeric strings using a generated symmetric key and display the ciphertext. Additionally, it should decrypt the ciphertext back to the original text when provided with the same key. The assignment requires a 5-minute presentation of the design and a working demonstration of the program, which should include at least two diffusion operations and two confusion operations to ensure the algorithm's robustness and utility in symmetric encryption.
+          Our team is tasked with discussing the fundamental characteristics that a robust symmetric encryption algorithm must possess. 
+          These characteristics include diffusion and confusion, and you need to design a didactic symmetric encryption algorithm that 
+          incorporates basic operations like bit shifting, transposition, substitution, XOR, and multiple rounds to enhance security. 
+          The algorithm should also consider reversible operations and involve complex operations like multiplication and key rotations 
+          if possible. After designing the algorithm, you'll need to implement it in a language of your choice, preferably one that supports
+           bit-level operations. The program should encrypt alphanumeric strings using a generated symmetric key and display the ciphertext. 
+           Additionally, it should decrypt the ciphertext back to the original text when provided with the same key. The assignment requires 
+           a 5-minute presentation of the design and a working demonstration of the program, which should include at least two diffusion 
+           operations and two confusion operations to ensure the algorithm's robustness and utility in symmetric encryption.
           </p>
           
           {/* Plain Text Field */}
@@ -19,24 +28,28 @@ export default function Home() {
               className="w-full h-24 px-16 border rounded-lg text-black focus:outline-none focus:ring focus:border-blue-300"
               placeholder="Enter plain text here..."
             ></textarea>
-
-          {/* Encrypted Text Field */}
-          <div className="text-lg my-4">
-            <p className="mb-2">Encrypted Text:</p>
+            <p className="mb-2">Encryption key:</p>
             <textarea
-              className="w-full h-24 px-16 border rounded-lg text-black focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Enter encrypted text here..."
+              className="w-mid h-12 px-4 border rounded-lg text-black focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="Enter plain text here..."
             ></textarea>
-            {/* Buttons Container */}
-            <div className="flex justify-center py-4 px-8 mt-4">
-              <button className="bg-green-500 text-white py-3 px-8 rounded-full hover:bg-green-600 transition duration-300 mr-4">
-                Encrypt
-              </button>
-              <button className="bg-red-500 text-white py-3 px-8 rounded-full hover:bg-red-600 transition duration-300 ml-4">
-                Decrypt
-              </button>
+          {/* Encrypted Text Field */}
+            <div className="text-lg my-4">
+              <p className="mb-2">Encrypted Text:</p>
+              <textarea
+                className="w-full h-24 px-16 border rounded-lg text-black focus:outline-none focus:ring focus:border-blue-300"
+                placeholder="Enter encrypted text here..."
+              ></textarea>
+              {/* Buttons Container */}
+              <div className="flex justify-center py-4 px-8 mt-4">
+                <button className="bg-green-500 text-white py-3 px-8 rounded-full hover:bg-green-600 transition duration-300 mr-4">
+                  Encrypt
+                </button>
+                <button className="bg-red-500 text-white py-3 px-8 rounded-full hover:bg-red-600 transition duration-300 ml-4">
+                  Decrypt
+                </button>
+              </div>
             </div>
-          </div>
           </div>
         </div>
       </main>
