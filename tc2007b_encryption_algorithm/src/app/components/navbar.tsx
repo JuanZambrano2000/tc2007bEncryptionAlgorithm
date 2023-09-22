374ae9cdd392f74b7495f63c0c9a69aec333ec12
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-21 20:15:39 -0600

    deployment version

--- tc2007b_encryption_algorithm/src/app/components/navbar.tsx
+++ tc2007b_encryption_algorithm/src/app/components/navbar.tsx
@@ -1,20 +1,29 @@
 import Link from 'next/link';
 import styles from './Navbar.module.css';
+import { useState } from 'react'; // Import useState from React
+
 const Navbar = () => {
+  // Create a state variable to track hover state
+  const [isHovered, setIsHovered] = useState(false);
+
+  // Define the class name for the link based on hover state
+  const linkClassName = isHovered ? styles['nav-link-hover'] : '';
+
   return (
     <nav className="bg-gray-800 p-4">
       <ul className="flex space-x-4 text-white">
         <li>
-          <Link href="/">Team members</Link>
+          {/* Apply the class name to the link */}
+          <Link href="/" className={linkClassName} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>Team members</Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href="/about" className={linkClassName} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>About</Link>
         </li>
         <li>
-          <Link href="/services">Services</Link>
+          <Link href="/services" className={linkClassName} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>Services</Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact" className={linkClassName} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>Contact</Link>
         </li>
       </ul>
     </nav>
