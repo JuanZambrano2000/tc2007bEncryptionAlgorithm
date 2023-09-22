e461ac7c072ad7ff379b8693f3f448fb875b9a9c
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-21 20:25:48 -0600

    Update page.tsx

--- tc2007b_encryption_algorithm/src/app/page.tsx
+++ tc2007b_encryption_algorithm/src/app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
           These characteristics include diffusion and confusion, and you need to design a didactic symmetric encryption algorithm that 
           incorporates basic operations like bit shifting, transposition, substitution, XOR, and multiple rounds to enhance security. 
           The algorithm should also consider reversible operations and involve complex operations like multiplication and key rotations 
-          if possible. After designing the algorithm, you'll need to implement it in a language of your choice, preferably one that supports
+          if possible. After designing the algorithm, you will need to implement it in a language of your choice, preferably one that supports
            bit-level operations. The program should encrypt alphanumeric strings using a generated symmetric key and display the ciphertext. 
            Additionally, it should decrypt the ciphertext back to the original text when provided with the same key. The assignment requires 
            a 5-minute presentation of the design and a working demonstration of the program, which should include at least two diffusion 
-           operations and two confusion operations to ensure the algorithm's robustness and utility in symmetric encryption.
+           operations and two confusion operations to ensure the algorithm robustness and utility in symmetric encryption.
           </p>
           <div className="flex justify-center"> {/* Added 'flex justify-center' class here */}
             <iframe
